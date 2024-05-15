c7341ea05799ffdcd6d943b66f28aed151cf0721
AndranikAdyan <andranikadyan@gmail.com>
2024-05-15 16:47:29 +0400

    delete input waiting

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 from functions import *
 
-playing = input("Let's play!\nEnter '1' or 'start' to continue, or '0' or 'exit' to exit: ")
+print("Let's play!")
 
-if check_input(playing):
-    if playing in ['1', 'start']:
-        dices = get_dices()
-        dices_sum = sum(dices)
 
-        if dices_sum == 7 or dices_sum == 11:
-            get_dices_sum(dices)
-            print("You Won!")
-        elif dices_sum == 2 or dices_sum == 3 or dices_sum == 12:
-            get_dices_sum(dices)
-            print("You lose :(")
-        else:
-            get_dices_sum(dices)
-            print(f"Now your goal number is {dices_sum}")
+dices = get_dices()
+dices_sum = sum(dices)
 
-            goal_number = dices_sum
-            while True:
-                dices = get_dices()
-                get_dices_sum(dices)
-                dices_sum = sum(dices)
+if dices_sum == 7 or dices_sum == 11:
+	get_dices_sum(dices)
+	print("You Won!")
+elif dices_sum == 2 or dices_sum == 3 or dices_sum == 12:
+	get_dices_sum(dices)
+	print("You lose :(")
+else:
+	get_dices_sum(dices)
+	print(f"Now your goal number is {dices_sum}")
 
-                if dices_sum == 7:
-                    print("You lose :(")
-                    break
-                elif dices_sum == goal_number:
-                    print("You Won!")
-                    break
-    elif playing in ['0', 'exit']:
-        print("Exiting the game.")
+	goal_number = dices_sum
+	while True:
+		dices = get_dices()
+		get_dices_sum(dices)
+		dices_sum = sum(dices)
+
+		if dices_sum == 7:
+			print("You lose :(")
+			break
+		elif dices_sum == goal_number:
+			print("You Won!")
+			break
