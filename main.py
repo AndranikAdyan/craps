d65566455d722dda5efec7e85a4e81f0b140b6f7
AndranikAdyan <andranikadyan@gmail.com>
2024-06-02 13:19:41 +0400

    update main

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ print("Let's play!")
 dices = get_dices()
 dices_sum = sum(dices)
 
+get_dices_sum(dices)
 if dices_sum == 7 or dices_sum == 11:
-	get_dices_sum(dices)
 	print("You Won!")
 elif dices_sum == 2 or dices_sum == 3 or dices_sum == 12:
-	get_dices_sum(dices)
 	print("You lose :(")
 else:
-	get_dices_sum(dices)
 	print(f"Now your goal number is {dices_sum}")
 
 	goal_number = dices_sum
